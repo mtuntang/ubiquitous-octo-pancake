56b9403f1b9ffffc6fe89ba32114ef56e0d4ce86
Hao Chen Lu <haochen.lu@gmail.com>
2019-11-30 15:18:55 -0800

    new src

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,11 @@
-public class Task {
+import java.util.Observable;
+
+public class Task extends Observable {
+    private String taskName;
+    private int average;
+    private int timesDone;
+
+    public Task(String name) {
+        taskName = name;
+    }
 }
