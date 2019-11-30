dd44c4061ef2a5cdab852ff843813c808bedfaa1
Hasan Siddiqui <Hasan.Siddiqui7777@gmail.com>
2019-11-30 15:44:09 -0800

    Added methods for basic task functionality such as attributes, load, save, start and endTime methods

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,13 +1,13 @@
 package model;
 
-import java.io.Serializable;
+import java.io.*;
 
 public class Task implements Serializable {
-    long startTime;
-    long endTime;
-    long avgTime;
-    String name;
-    int numberOfTimesRun;
+    private long startTime;
+    private long endTime;
+    private long avgTime;
+    private String name;
+    private int numberOfTimesRun;
 
     public Task(String name){
         this.name = name;
@@ -18,14 +18,29 @@ public class Task implements Serializable {
     }
 
     public void endTime(){
-
+        endTime = System.nanoTime();
+        numberOfTimesRun++;
     }
 
     public void update(){
-
+        avgTime = (endTime - startTime)/numberOfTimesRun;
     }
 
-    public void save(){
+    public void load() throws IOException, ClassNotFoundException {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("stub.dat"));
+        Task load = (Task)in.readObject();
+        loadFields(load);
+    }
 
+    public void loadFields(Task task) throws IOException{
+        this.name = task.name;
+        this.avgTime = task.avgTime;
+        this.numberOfTimesRun = task.numberOfTimesRun;
+    }
+
+    public void save() throws IOException {
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("stub.dat"));
+        out.writeObject(this);
+        out.close();
     }
 }
