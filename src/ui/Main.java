80bbb7536265bd76e1346a21d809b98445761779
Hao Chen Lu <haochen.lu@gmail.com>
2019-11-30 15:45:31 -0800

    main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,4 +1,39 @@
 package ui;
+import model.TaskHandler;
+
+import java.util.Scanner;
 
 public class Main {
+    TaskHandler taskHandler = new TaskHandler();
+
+    public static void main(String[] args) {
+        welcome();
+        while (true) {
+
+        }
+    }
+
+    private static void welcome() {
+    }
+
+    public static void queryTask() {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Name your task: ");
+        String taskName = myObj.nextLine();  // Read user input
+        taskHandler.addTask(taskname);
+    }
+
+    public static void queryTask() {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Name your task: ");
+        String taskName = myObj.nextLine();  // Read user input
+        taskHandler.addTask(taskname);
+    }
+
+    public static void removeTask() {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Which task would you like to remove?: ");
+        String taskName = myObj.nextLine();  // Read user input
+        taskHandler.removeTask(taskname);
+    }
 }
