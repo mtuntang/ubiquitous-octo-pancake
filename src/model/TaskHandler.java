6f89e74ebef8e1dc04356a6cf90df200bade56b1
mtuntang <mtuntang.student.ubc.ca>
2019-11-30 15:59:22 -0800

    save and load methods

--- src/model/TaskHandler.java
+++ src/model/TaskHandler.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,10 +15,39 @@ public class TaskHandler {
     }
 
     public void addTask(String name) {
-        taskMap.put(new Task(name));
+        taskMap.put(name, new Task(name));
+    }
+
+    public void remove(String name) {
+        taskMap.remove(name);
     }
 
     public Map getMap() {
         return taskMap;
    }
+    public void save(TaskHandler taskHandler) {
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("save.dat"));
+            out.writeObject(this);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //REQUIRES : save.txt exists in model package
+    //EFFECTS : loads save data, makes a pointer to local TotalExpenses field
+    public TaskHandler load(TaskHandler taskHandler) {
+        try {
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("save.dat"));
+            taskHandler = (TaskHandler) in.readObject();
+            in.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            return taskHandler;
+        } finally {
+            return taskHandler;
+        }
+    }
 }
